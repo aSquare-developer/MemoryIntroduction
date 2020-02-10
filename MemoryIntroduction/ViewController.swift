305408dc1f05f14f0fff03ce23ccf124dc4313fb
Artur <artur.anissimov@gmail.com>
2020-02-10 19:14:27 +0200

    Memory Introduction

--- MemoryIntroduction/ViewController.swift
+++ MemoryIntroduction/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var first: MyClass? = MyClass()
+        var second = first
+        var third = first
+        
+        first = nil
+        second = nil
+        third = nil
     }
 
 
 }
 
+class MyClass {
+    
+    var someNumber = 16
+    
+    // deinit выполняется когда объект выгружается из памяти.
+    deinit {
+        print("MyClass will be dealocated!")
+    }
+}
